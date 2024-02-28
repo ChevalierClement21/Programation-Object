3864a902446305d9de81a3b5ef54ffeb27980ab7
Clément.Chevalier <clementchevalier2102@outlook.fr>
2024-02-28 14:53:06 +0100

    ajout de la page contact + navbar

--- navbar.php
+++ navbar.php
@@ -32,6 +32,9 @@
                     <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="Accueil.php">Accueil</a>
                     </li>
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="contact.php">Contact</a>
+                    </li>
                 </ul>
                 </div>
             </div>
